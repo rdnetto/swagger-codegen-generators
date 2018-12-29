5f78db4777f5936c3986113bc80c115340c33bfb
Reuben D'Netto <rdnetto@gmail.com>
2018-12-29 22:53:31 +1100

    Fixed Java warnings (incl. broken String.format() invocation)

--- src/main/java/io/swagger/codegen/v3/generators/haskell/HaskellServantCodegen.java
+++ src/main/java/io/swagger/codegen/v3/generators/haskell/HaskellServantCodegen.java
@@ -123,7 +123,7 @@ public class HaskellServantCodegen extends DefaultCodegenConfig implements Codeg
      * Language Specific Primitives.  These types will not trigger imports by
      * the client generator
      */
-        languageSpecificPrimitives = new HashSet<String>(
+        languageSpecificPrimitives = new HashSet<>(
                 Arrays.asList(
                         "Bool",
                         "String",
@@ -215,14 +215,14 @@ public class HaskellServantCodegen extends DefaultCodegenConfig implements Codeg
         String[] words = title.split(" ");
 
         // The package name is made by appending the lowercased words of the title interspersed with dashes
-        List<String> wordsLower = new ArrayList<String>();
+        List<String> wordsLower = new ArrayList<>();
         for (String word : words) {
             wordsLower.add(word.toLowerCase());
         }
         String cabalName = joinStrings("-", wordsLower);
 
         // The API name is made by appending the capitalized words of the title
-        List<String> wordsCaps = new ArrayList<String>();
+        List<String> wordsCaps = new ArrayList<>();
         for (String word : words) {
             wordsCaps.add(firstLetterToUpper(word));
         }
@@ -267,7 +267,7 @@ public class HaskellServantCodegen extends DefaultCodegenConfig implements Codeg
             return String.format("[%s]", getTypeDeclaration(inner));
         } else if (propertySchema instanceof MapSchema && hasSchemaProperties(propertySchema)) {
             Schema inner = (Schema) propertySchema.getAdditionalProperties();
-            return String.format("Map.Map String ", getTypeDeclaration(inner));
+            return "Map.Map String " + getTypeDeclaration(inner);
         }
         return fixModelChars(super.getTypeDeclaration(propertySchema));
     }
@@ -281,13 +281,13 @@ public class HaskellServantCodegen extends DefaultCodegenConfig implements Codeg
      */
     @Override
     public String getSchemaType(Schema schema) {
-        String swaggerType = super.getSchemaType(schema);
-        String type = null;
+        final String swaggerType = super.getSchemaType(schema);
+        final String type;
         if (typeMapping.containsKey(swaggerType)) {
             type = typeMapping.get(swaggerType);
             if (languageSpecificPrimitives.contains(type))
                 return toModelName(type);
-        } else if(swaggerType == "object") {
+        } else if(swaggerType.equals("object")) {
             type = "Value";
         } else if(typeMapping.containsValue(swaggerType)) {
             type = swaggerType + "_";
@@ -310,10 +310,9 @@ public class HaskellServantCodegen extends DefaultCodegenConfig implements Codeg
             return "(Map.Map Text " + inner + ")";
         } else if (propertySchema instanceof ArraySchema) {
             ArraySchema arraySchema = (ArraySchema) propertySchema;
-            String inner = getSchemaType(arraySchema.getItems());
             // Return only the inner type; the wrapping with QueryList is done
             // somewhere else, where we have access to the collection format.
-            return inner;
+            return getSchemaType(arraySchema.getItems());
         } else {
             return null;
         }
@@ -338,7 +337,7 @@ public class HaskellServantCodegen extends DefaultCodegenConfig implements Codeg
     // IdType is provided by the capture params.
     private List<String> pathToServantRoute(String path, List<CodegenParameter> pathParams) {
         // Map the capture params by their names.
-        HashMap<String, String> captureTypes = new HashMap<String, String>();
+        HashMap<String, String> captureTypes = new HashMap<>();
         for (CodegenParameter param : pathParams) {
             captureTypes.put(param.baseName, param.dataType);
         }
@@ -349,7 +348,7 @@ public class HaskellServantCodegen extends DefaultCodegenConfig implements Codeg
         }
 
         // Convert the path into a list of servant route components.
-        List<String> pathComponents = new ArrayList<String>();
+        List<String> pathComponents = new ArrayList<>();
         for (String piece : path.split("/")) {
             if (piece.startsWith("{") && piece.endsWith("}")) {
                 String name = piece.substring(1, piece.length() - 1);
@@ -366,7 +365,7 @@ public class HaskellServantCodegen extends DefaultCodegenConfig implements Codeg
     // Extract the arguments that are passed in the route path parameters
     private List<String> pathToClientType(String path, List<CodegenParameter> pathParams) {
         // Map the capture params by their names.
-        HashMap<String, String> captureTypes = new HashMap<String, String>();
+        HashMap<String, String> captureTypes = new HashMap<>();
         for (CodegenParameter param : pathParams) {
             captureTypes.put(param.baseName, param.dataType);
         }
@@ -377,7 +376,7 @@ public class HaskellServantCodegen extends DefaultCodegenConfig implements Codeg
         }
 
         // Convert the path into a list of servant route components.
-        List<String> type = new ArrayList<String>();
+        List<String> type = new ArrayList<>();
         for (String piece : path.split("/")) {
             if (piece.startsWith("{") && piece.endsWith("}")) {
                 String name = piece.substring(1, piece.length() - 1);
@@ -441,7 +440,7 @@ public class HaskellServantCodegen extends DefaultCodegenConfig implements Codeg
         if (returnType == null || returnType.equals("null")) {
             returnType = "()";
         }
-        if (returnType.indexOf(" ") >= 0) {
+        if (returnType.contains(" ")) {
             returnType = "(" + returnType + ")";
         }
         path.add("Verb '" + op.httpMethod.toUpperCase() + " 200 '[JSON] " + returnType);
@@ -476,8 +475,8 @@ public class HaskellServantCodegen extends DefaultCodegenConfig implements Codeg
         String name = string;
         //Check if it is a reserved word, in which case the underscore is added when property name is generated.
         if (string.startsWith("_")) {
-            if (reservedWords.contains(string.substring(1, string.length()))) {
-                name = string.substring(1, string.length());
+            if (reservedWords.contains(string.substring(1))) {
+                name = string.substring(1);
             } else if (reservedWordsMappings.containsValue(string)) {
                 name = LEADING_UNDERSCORE.matcher(string).replaceFirst("");
             }
